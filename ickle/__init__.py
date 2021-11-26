f7d3cb085bab7329cc7942d23f581ff9dcef60a8
karishmas6 <carishmashukla28@gmail.com>
2021-11-26 21:20:06 +0530

    method: check_input_types

--- ickle/__init__.py
+++ ickle/__init__.py
@@ -10,6 +10,18 @@ class DataFrame:
         Create it by passing a dictionary of NumPy arrays to the values parameter.
 
         Parameters
-        1. data (dict) : A dictionary of strings mapped to NumPy arrays. The key will
+        1. data (dict): A dictionary of strings mapped to NumPy arrays. The key will
         become the column name.
         """
+
+    def _check_input_types(self,data):
+        if not isinstance(data, dict):
+            raise TypeError('`data` can accept only dictionaries')
+        for key, value in data.items():
+            if not isinstance(key, str):
+                raise TypeError('keys of `data` must be of type str')
+            if not isinstance(value, np.ndarray):
+                raise TypeError('values of `data` must be NumPy arrays')
+            if value.ndim != 1:
+                raise ValueError('values of `data` must be some one-dimensional array')
+
