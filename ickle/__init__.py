4a79c2a4785a96a32f25a89231258c94919d978c
karishmas6 <carishmashukla28@gmail.com>
2021-12-15 16:47:36 +0530

    add aggregation methods

--- ickle/__init__.py
+++ ickle/__init__.py
@@ -334,6 +334,60 @@ class DataFrame:
         """
         return self[-n:, :]
 
+    ### Aggregation Methods ###
+
+    def min(self):
+        return self._agg(np.min)
+
+    def max(self):
+        return self._agg(np.max)
+
+    def mean(self):
+        return self._agg(np.mean)
+
+    def median(self):
+        return self._agg(np.median)
+
+    def sum(self):
+        return self._agg(np.sum)
+
+    def var(self):
+        return self._agg(np.var)
+
+    def std(self):
+        return self._agg(np.std)
+
+    def all(self):
+        return self._agg(np.all)
+
+    def any(self):
+        return self._agg(np.any)
+
+    def argmax(self):
+        return self._agg(np.argmax)
+
+    def argmin(self):
+        return self._agg(np.argmin)
+
+    def _agg(self, aggfunc):
+        """
+        Generic aggregation function that applies the aggregation to each column
+
+        Parameters
+        ---------
+        aggfunc: str of the aggregation function name in NumPy
+
+        Returns
+        -------
+        DataFrame
+        """
+        new_data = {}
+        for col, value in self._data.items():
+            try: 
+                new_data[col] = np.array([aggfunc(value)])
+            except TypeError:
+                pass
+        return DataFrame(new_data)
 
 
 
