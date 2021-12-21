79319f6227ff538c44588249c1d5d5a0eab6bdd7
karishmas6 <carishmashukla28@gmail.com>
2021-12-21 20:14:26 +0530

    add arithmetic operations

--- ickle/__init__.py
+++ ickle/__init__.py
@@ -662,3 +662,84 @@ class DataFrame:
                 value[n:] = np.nan
             return value / value_shifted
         return self._non_agg(func)
+
+    ### Arithmetic and Comparison Operators ###
+    # https://docs.python.org/3/reference/datamodel.html#emulating-numeric-type
+
+    def __add__(self, other):
+        return self._oper('__add__', other)
+
+    def __radd__(self, other):
+        return self._oper('__radd__', other)
+
+    def __sub__(self, other):
+        return self._oper('__sub__', other)
+
+    def __rsub__(self, other):
+        return self._oper('__rsub__', other)
+
+    def __mul__(self, other):
+        return self._oper('__mul__', other)
+
+    def __rmul__(self, other):
+        return self._oper('__rmul__', other)
+
+    def __truediv__(self, other):
+        return self._oper('__truediv__', other)
+
+    def __rtruediv__(self, other):
+        return self._oper('__rtruediv__', other)
+
+    def __floordiv__(self, other):
+        return self._oper('__floordiv__', other)
+
+    def __rfloordiv__(self, other):
+        return self._oper('__rfloordiv__', other)
+
+    def __pow__(self, other):
+        return self._oper('__pow__', other)
+
+    def __rpow__(self, other):
+        return self._oper('__rpow__', other)
+
+    def __gt__(self, other):
+        return self._oper('__gt__', other)
+
+    def __lt__(self, other):
+        return self._oper('__lt__', other)
+
+    def __ge__(self, other):
+        return self._oper('__ge__', other)
+
+    def __le__(self, other):
+        return self._oper('__le__', other)
+
+    def __ne__(self, other):
+        return self._oper('__ne__', other)
+
+    def __eq__(self, other):
+        return self._oper('__eq__', other)
+
+    def _oper(self, op, other):
+        """
+        Generic operator method
+
+        Parameters
+        ----------
+        op: str Name of special method
+        other: the other object being operated on
+
+        Returns
+        -------
+        A DataFrame
+        """
+        if isinstance(other, DataFrame):
+            if other.shape[1] != 1:
+                raise ValueError('`other` must be a one-column DataFrame')
+            other = next(iter(other._data.values()))
+        new_data = {}
+        for col, value in self._data.items():
+            func = getattr(value, op)
+            new_data[col] = func(other)
+        return DataFrame(new_data)
+
