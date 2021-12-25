c8a2970abc46d0d63b1335d65a3e181144b55c68
karishmas6 <carishmashukla28@gmail.com>
2021-12-25 18:20:40 +0530

    add string-only methods

--- ickle/__init__.py
+++ ickle/__init__.py
@@ -24,6 +24,10 @@ class DataFrame:
         # Convert unicode arrays to objects
         self._data = self._convert_unicode_to_object(data)
 
+        # Allow for special methods for strings
+        self.str = StringMethods(self)
+        self._add_docs()
+
     def _check_input_types(self,data):
         if not isinstance(data, dict):
             raise TypeError('`data` can accept only dictionaries')
@@ -912,4 +916,93 @@ class DataFrame:
         """
         for name in agg_names:
             getattr(DataFrame, name).__doc__ = agg_doc.format(name)
-            
+
+class StringMethods:
+
+    def __init__(self, df):
+        self._df = df
+
+    def capitalize(self, col):
+        return self._str_method(str.capitalize, col)
+
+    def center(self, col, width, fillchar=None):
+        if fillchar is None:
+            fillchar = ' '
+        return self._str_method(str.center, col, width, fillchar)
+
+    def count(self, col, sub, start=None, stop=None):
+        return self._str_method(str.count, col, sub, start, stop)
+
+    def endswith(self, col, suffix, start=None, stop=None):
+        return self._str_method(str.endswith, col, suffix, start, stop)
+
+    def startswith(self, col, suffix, start=None, stop=None):
+        return self._str_method(str.startswith, col, suffix, start, stop)
+
+    def find(self, col, sub, start=None, stop=None):
+        return self._str_method(str.find, col, sub, start, stop)
+
+    def len(self, col):
+        return self._str_method(str.__len__, col)
+
+    def get(self, col, item):
+        return self._str_method(str.__getitem__, col, item)
+
+    def index(self, col, sub, start=None, stop=None):
+        return self._str_method(str.index, col, sub, start, stop)
+
+    def isalnum(self, col):
+        return self._str_method(str.isalnum, col)
+
+    def isalpha(self, col):
+        return self._str_method(str.isalpha, col)
+
+    def isdecimal(self, col):
+        return self._str_method(str.isdecimal, col)
+
+    def islower(self, col):
+        return self._str_method(str.islower, col)
+
+    def isnumeric(self, col):
+        return self._str_method(str.isnumeric, col)
+
+    def isspace(self, col):
+        return self._str_method(str.isspace, col)
+
+    def istitle(self, col):
+        return self._str_method(str.istitle, col)
+
+    def isupper(self, col):
+        return self._str_method(str.isupper, col)
+
+    def lstrip(self, col, chars):
+        return self._str_method(str.lstrip, col, chars)
+
+    def rstrip(self, col, chars):
+        return self._str_method(str.rstrip, col, chars)
+
+    def strip(self, col, chars):
+        return self._str_method(str.strip, col, chars)
+
+    def replace(self, col, old, new, count=None):
+        if count is None:
+            count = -1
+        return self._str_method(str.replace, col, old, new, count)
+
+    def swapcase(self, col):
+        return self._str_method(str.swapcase, col)
+
+    def title(self, col):
+        return self._str_method(str.title, col)
+
+    def lower(self, col):
+        return self._str_method(str.lower, col)
+
+    def upper(self, col):
+        return self._str_method(str.upper, col)
+
+    def zfill(self, col, width):
+        return self._str_method(str.zfill, col, width)
+
+    def encode(self, col, encoding='utf-8', errors='strict'):
+        return self._str_method(str.encode, col, encoding, errors)
